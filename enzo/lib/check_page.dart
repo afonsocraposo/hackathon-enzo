05ed257bdb25ff03a79acc36f2b2e3eef07a5e5e
carlos.pereira <carlosfcdp94@hotmail.com>
2021-11-26 16:36:12 +0000

    check and warning pages

---
+++ enzo/lib/check_page.dart
@@ -0,0 +1,63 @@
+import 'package:enzo/metrics_page.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class CheckPage extends StatefulWidget {
+  const CheckPage({Key? key}) : super(key: key);
+
+  @override
+  _CheckPageState createState() => _CheckPageState();
+}
+
+class _CheckPageState extends State<CheckPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Fluttertoast.showToast(
+        msg: "Esta tudo OK",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 20.0
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(70),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: Center(
+                            child: Icon(
+                          Icons.check,
+                          size: 150,
+                        ))),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.green, shape: const CircleBorder()),
+                  ),
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ enzo/lib/warning_page.dart
@@ -0,0 +1,60 @@
+import 'package:enzo/metrics_page.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class WarningPage extends StatefulWidget {
+  const WarningPage({Key? key}) : super(key: key);
+
+  @override
+  _WarningPageState createState() => _WarningPageState();
+}
+
+class _WarningPageState extends State<WarningPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Fluttertoast.showToast(
+        msg: "Nao deve fazer desporto!",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 20.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(70),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: Center(
+                            child: Icon(
+                          Icons.warning,
+                          size: 150,
+                        ))),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.red, shape: const CircleBorder()),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
